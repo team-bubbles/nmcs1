5a7092f8af82396c4f4f5848b8b5ae2014db338c
lcb931023 <lcb931023@gmail.com>
2014-10-30 02:58:53 -0400

    added autoprefiuxer to sass gulp task

--- Template/gulp/tasks/sass.js
+++ Template/gulp/tasks/sass.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var handleErrors = require('../util/handleErrors');
+var postcss      = require('gulp-postcss');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer-core');
 var config=require('../config').sass;
 
 gulp.task('sass', ['images'], function () {
@@ -12,5 +15,8 @@ gulp.task('sass', ['images'], function () {
       sourcemapPath: '../sass'
     }))
     .on('error', handleErrors)
+    .pipe(sourcemaps.init())
+    .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(config.dest));
 });
