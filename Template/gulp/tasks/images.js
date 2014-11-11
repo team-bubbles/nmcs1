a526667416ba197b6f841c15a80038f4482b5e45
lcb931023 <lcb931023@gmail.com>
2014-11-11 15:52:48 -0500

    update imagemin even though it sucks butt

--- Template/gulp/tasks/images.js
+++ Template/gulp/tasks/images.js
@@ -1,11 +1,11 @@
 var changed    = require('gulp-changed');
 var gulp       = require('gulp');
-// var imagemin   = require('gulp-imagemin'); //[HACK] removed for now because dependency bugs
+var imagemin   = require('gulp-imagemin');
 var config     = require('../config').images;
 
 gulp.task('images', function() {
   return gulp.src(config.src)
     .pipe(changed(config.dest)) // Ignore unchanged files
-    //.pipe(imagemin()) // Optimize [HACK] removed for now because dependency bugs
+    .pipe(imagemin()) // Optimize
     .pipe(gulp.dest(config.dest));
 });
