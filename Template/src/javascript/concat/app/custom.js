955a1b630d58d8930ca384869c1b768127c0df32
lcb931023 <lcb931023@gmail.com>
2014-12-12 17:08:00 -0500

    anal style change

--- Template/src/javascript/concat/app/custom.js
+++ Template/src/javascript/concat/app/custom.js
@@ -1,28 +1,28 @@
 
 (function(){
-    'use strict';
-    var ww = window.innerWidth;
-    var wh = window.innerHeight;
+  'use strict';
+  var ww = window.innerWidth;
+  var wh = window.innerHeight;
 
-    function init(){
-        setupUI();
-    }
+  function init(){
+    setupUI();
+  }
 
-    function setupUI(){
-    //Overlay Buttons
-        document.querySelector("#menuBTN").onclick = function(){
+  function setupUI(){
+  //Overlay Buttons
+    document.querySelector("#menuBTN").onclick = function(){
 
-            if(this.className == ""){
-                document.getElementById("prBox").className = "overlayMenu open";
-                this.className = "active";
-            }
-            else if (this.className == "active"){
-                document.getElementById("prBox").className = "overlayMenu";
-                this.className = "";
-            }
-        };
+      if(this.className == ""){
+        document.getElementById("prBox").className = "overlayMenu open";
+        this.className = "active";
+      }
+      else if (this.className == "active"){
+        document.getElementById("prBox").className = "overlayMenu";
+        this.className = "";
+      }
+    };
 
-    }
+  }
 
   window.addEventListener("load", init);
 }());
